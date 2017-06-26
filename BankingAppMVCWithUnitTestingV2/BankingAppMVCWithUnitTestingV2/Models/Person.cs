15b62c24499476d6149a8bf3c4fae8585fb76333
Mike Daughtrey <mikie.daughtrey@gmail.com>
2017-06-26 14:05:53 +0200

    Made a model for Person

---
+++ BankingAppMVCWithUnitTestingV2/BankingAppMVCWithUnitTestingV2/Models/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BankingAppMVCWithUnitTestingV2.Models
+{
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [MaxLength(140, ErrorMessage = "Name is too long, try again")]
+        public string Fname { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [MaxLength(140, ErrorMessage = "Name is too long, try again")]
+        public string Lname { get; set; }
+
+        [Required(ErrorMessage = "Personal Number is required")]
+        [DisplayName("Person Nummer")]
+        public int PersonNum { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime BirthDate { get; set; }
+    }
+}
