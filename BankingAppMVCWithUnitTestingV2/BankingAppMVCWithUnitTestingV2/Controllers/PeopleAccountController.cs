f264c66bb182291591d508db33ac83ea7b4dd554
Mike Daughtrey <mikie.daughtrey@gmail.com>
2017-06-27 09:53:52 +0200

    Working on getting accounts to display.

---
+++ BankingAppMVCWithUnitTestingV2/BankingAppMVCWithUnitTestingV2/Controllers/PeopleAccountController.cs
@@ -0,0 +1,24 @@
+﻿using BankingAppMVCWithUnitTestingV2.Models;
+using BankingAppMVCWithUnitTestingV2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankingAppMVCWithUnitTestingV2.Controllers
+{
+    public class PeopleAccountController : Controller
+    {
+        AccountRepository ar = new AccountRepository();
+        PersonRepository pr = new PersonRepository();
+
+        // GET: PeopleAccount
+        public ActionResult GetAccounts(int? personID)
+        {
+            Person person = pr.Person(personID);
+            IEnumerable<Account> accounts = person.Accounts.ToList();
+            return View(accounts);
+        }
+    }
+}
